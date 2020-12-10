81cadad401473ff1828513131656f1cc1163ce4f
Rosie Ivanova <rosie@bubblebubble.me>
2020-12-10 21:25:49 +0000

    fix day6 part 2

--- day6/main.py
+++ day6/main.py
@@ -12,20 +12,18 @@ print(sum(count_of_yes_questions))
 
 ######## PART 2 ##########
 
-
 input_split = [{'user_answers': answers.split('\n'), 'set_of_yes': to_set(answers)} for answers in input]
-
 
 def exists(question, user_answer):
     return True if question in user_answer else False
 
 def check_user_answers(answers):
-    all_yes = []
+    all_yes_count = 0
     for question in answers['set_of_yes']:
-        if all(exists(question, user_answer) for user_answer in answers['user_answers']):
-            all_yes.extend(question)
-    return all_yes
+        if all(exists(question, user_answer) for user_answer in answers['user_answers'] if user_answer):
+            all_yes_count = all_yes_count + 1
+    return all_yes_count
 
-count_of_yes_question_all_users = [len(check_user_answers(answers)) for answers in input_split]
+count_of_yes_question_all_users = [check_user_answers(answers) for answers in input_split]
 
 print(sum(count_of_yes_question_all_users))
