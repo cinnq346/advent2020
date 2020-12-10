213cabee64f894d5e53f50ccab55cc813b1be953
Rosie Ivanova <rosie@bubblebubble.me>
2020-12-10 21:00:43 +0000

    WIP day6 part 2

--- day6/main.py
+++ day6/main.py
@@ -9,3 +9,23 @@ def to_set(answers):
 count_of_yes_questions = [len(to_set(answers)) for answers in input]
 
 print(sum(count_of_yes_questions))
+
+######## PART 2 ##########
+
+
+input_split = [{'user_answers': answers.split('\n'), 'set_of_yes': to_set(answers)} for answers in input]
+
+
+def exists(question, user_answer):
+    return True if question in user_answer else False
+
+def check_user_answers(answers):
+    all_yes = []
+    for question in answers['set_of_yes']:
+        if all(exists(question, user_answer) for user_answer in answers['user_answers']):
+            all_yes.extend(question)
+    return all_yes
+
+count_of_yes_question_all_users = [len(check_user_answers(answers)) for answers in input_split]
+
+print(sum(count_of_yes_question_all_users))
