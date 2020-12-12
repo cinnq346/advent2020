4078045caf817952b8e3da7d4e825fabc3b8d21a
Rosie Ivanova <rosie@bubblebubble.me>
2020-12-12 22:40:51 +0000

    day8 part 2

--- day8/main.py
+++ day8/main.py
@@ -40,31 +40,28 @@ def clean(rule):
 input_enum = {idx: clean(rule) for idx, rule in enumerate(input)}
 
 def endless_recursion_with_exit(idx, acc, temp_input_enum, rules_already_seen):
+    rules_already_seen.append(idx)
     action, value = temp_input_enum[idx]
 
-    rules_already_seen.append(idx)
+    next_idx = idx + int(value) if action == 'jmp' else idx + 1
+
+    if next_idx > max(list(temp_input_enum.keys())):
+        print(acc)
+        return
 
     if len(rules_already_seen) > len(set(rules_already_seen)):
         return
 
-    if ((action in ['acc', 'nop']) and (idx == len(input_enum))):
-        print(acc)
-        return
-
     if action == 'acc':
         acc = acc + int(value)
-        endless_recursion_with_exit(idx + 1, acc, temp_input_enum, rules_already_seen)
-    elif action == 'jmp':
-        endless_recursion_with_exit(idx + int(value), acc, temp_input_enum, rules_already_seen)
-    elif action == 'nop':
-        endless_recursion_with_exit(idx + 1, acc, temp_input_enum, rules_already_seen)
-
+    endless_recursion_with_exit(next_idx, acc, temp_input_enum, rules_already_seen)
 
 for idx, rule in input_enum.items():
-    temp_input_enum = input_enum
+    temp_input_enum = input_enum.copy()
     action, value = rule
     if action == 'jmp':
         temp_input_enum[idx] = ('nop', value)
     elif action == 'nop':
         temp_input_enum[idx] = ('jmp', value)
+
     endless_recursion_with_exit(0, 0, temp_input_enum, [])
